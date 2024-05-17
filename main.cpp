2a0a3ac7c4d9574d34cb931fdfa704ea77ed29a7
lukeforte40 <lukeforte40@gmail.com>
2024-05-17 00:26:43 -0400

    added functions to the main.cpp for final submission.

--- main.cpp
+++ main.cpp
@@ -8,8 +8,17 @@ int main(){
         cout << "Class Title: Scripting and Programming - Applications - C867 \t" << "Language Used: C++" << endl;
         cout << "Student ID: 010398679 \t" << "Student Name: Luke Forte" << endl;
         Roster* classRoster = new Roster;
+        // Add student objects
         classRoster->printAll();
-        // Add code here
+        // classRoster->printInvalidEmails();
+        // loop through classRosterArray and for each element:
+        //classRoster.printAverageDaysInCourse(/*current_object's student id*/);
+        // classRoster->printByDegreeProgram(SOFTWARE);
+        classRoster->remove("A3");
+        classRoster->printAll();
+        classRoster->remove("A3");
+        // Testing
+            // classRoster->add("A1", "Luke", "Forte", "test@email.com", 25, 10, 30, 15, SOFTWARE);
         delete classRoster;
         return 0;
 };
